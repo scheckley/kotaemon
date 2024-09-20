28ee74d912de857bab259bee0bdcb4863ffc9c67
Stephen Checkley <stephen.checkley@stfc.ac.uk>
2024-09-20 17:17:13 +0100

    updated app.py with IP and port

--- app.py
+++ app.py
@@ -17,8 +17,6 @@ demo = app.make()
 demo.queue().launch(
     favicon_path=app._favicon,
     inbrowser=True,
-    server_name='0.0.0.0',
-    server_port='8443',
     allowed_paths=[
         "libs/ktem/ktem/assets",
         GRADIO_TEMP_DIR,
